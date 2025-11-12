ea57b693ec79e645e8d9a6ca9ff387301022f8f0
VicBrienen <2x5chmddbj@privaterelay.appleid.com>
2025-11-12 13:20:08 +0100

    implement apply function

--- microtorch/autograd.py
+++ microtorch/autograd.py
@@ -1,4 +1,5 @@
 import numpy as np
+from tensor import Tensor
 
 class Operation:
     def __init__(self, *parents, **attributes):
@@ -8,3 +9,12 @@ class Operation:
 
     def cache_for_backward(self, *xs):
         self.forward_cache = xs
+
+def apply(operation, *parents, **attributes):
+    op = operation(*parents, **attributes)                              # creates operation object
+    input = [tensor.data for tensor in parents]                         # extract operation inputs from parent tensors as raw numpy arrays
+    requires_grad = any(tensor.requires_grad for tensor in parents)     # determine if any parent requires a gradient
+    output = Tensor(op.forward(*input), requires_grad=requires_grad)    # wrap result in a tensor
+    if requires_grad:
+        output.grad_fn = op # store operation that created output tensor
+    return output
