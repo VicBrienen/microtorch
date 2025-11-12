c0f6e2a07552632d6d5abcd5b09cbbe2f1662741
VicBrienen <2x5chmddbj@privaterelay.appleid.com>
2025-11-12 21:13:05 +0100

    implement Tensor operation wrappers

--- microtorch/tensor.py
+++ microtorch/tensor.py
@@ -1,4 +1,5 @@
 import numpy as np
+import autograd as ag
 
 class Tensor:
     def __init__(
@@ -6,6 +7,7 @@ class Tensor:
         requires_grad=False,
         dtype=np.float32
         ):
+
         array = np.array(data, dtype=dtype, copy=True)                      # temporary array for readout
         self.data = array.reshape(-1)                                       # static 1D storage
         self.shape = tuple(array.shape)                                     # matrix shape
@@ -15,3 +17,24 @@ class Tensor:
         self.requires_grad = requires_grad                                  # trainable parameters or not
         self.grad = None                                                    # tensor that stores accumulated gradients
         self.grad_fn = None                                                 # backward function or None for leaf tensor
+
+    def __add__(self, other):
+        return ag.apply(ag.Add, self, other)
+
+    def __mul__(self, other):
+        return ag.apply(ag.Mul, self, other)
+
+    def __matmul__(self, other):
+        return ag.apply(ag.MatMul, self, other)
+
+    def __neg__(self):
+        return ag.apply(ag.Neg, self)
+
+    def __sub__(self, other):
+        return ag.apply(ag.Add, self, ag.apply(ag.Neg, other))
+
+    def sum(self):
+        return ag.apply(ag.Sum, self)
+
+    def relu(self):
+        return ag.apply(ag.ReLU, self)
