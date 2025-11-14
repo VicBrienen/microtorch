cd7bb3274a19e5a2e9e78e8514337ab6ac778d2c
VicBrienen <2x5chmddbj@privaterelay.appleid.com>
2025-11-14 12:59:53 +0100

    abstract low-level tensor operations

--- microtorch/tensor.py
+++ microtorch/tensor.py
@@ -7,12 +7,7 @@ class Tensor:
         requires_grad=False,
         dtype=np.float32
         ):
-
-        array = np.array(data, dtype=dtype, copy=True)                      # temporary array for readout
-        self.data = array.reshape(-1)                                       # static 1D storage
-        self.shape = tuple(array.shape)                                     # matrix shape
-        self.stride = tuple(s // array.itemsize for s in array.strides)     # element wise stride from bit wise stride
-        self.offset = 0                                                     # assume 0 offset
+        self.data = np.array(data, dtype=dtype, copy=True)                  # original numpy array (no static 1d storage)
         self.dtype = dtype                                                  # store data type
         self.requires_grad = requires_grad                                  # trainable parameters or not
         self.grad = None                                                    # tensor that stores accumulated gradients
