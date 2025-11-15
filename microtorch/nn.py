43dba1d7c9278ab5360b21dd98caddd20fbd87e6
VicBrienen <2x5chmddbj@privaterelay.appleid.com>
2025-11-15 18:58:57 +0100

    Implement Module parent class

--- microtorch/nn.py
+++ microtorch/nn.py
@@ -1,7 +1,15 @@
 from tensor import Tensor
 import numpy as np
 
-class Linear:
+class Module:
+    def parameters(self):
+        return []
+    
+    def zero_grad(self):
+        for p in self.parameters():
+            p.grad = None
+
+class Linear(Module):
     def __init__(self, in_features, out_features):
         w = np.random.randn(in_features, out_features).astype(np.float32)
         b = np.zeros(out_features, dtype=np.float32)
@@ -14,7 +22,3 @@ class Linear:
     
     def parameters(self):
         return [self.w, self.b]
-
-class ReLU:
-    def __call__(self, x):
-        return x.relu()
