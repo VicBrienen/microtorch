4023dc608ca6bc55e1d5689f0a840d515d0b0ba0
VicBrienen <2x5chmddbj@privaterelay.appleid.com>
2025-11-16 13:16:11 +0100

    implement cross_entropy function

--- microtorch/nn/functional.py
+++ microtorch/nn/functional.py
@@ -4,16 +4,15 @@ from tensor import Tensor
 def relu(x):
     return x.maximum(Tensor(0.0, dtype=x.dtype))
 
-def sigmoid(x):
-    return 1 / (1 + (-x).exp())
-
-def tanh(x):
-    e_pos = x.exp()
-    e_neg = (-x).exp()
-    return (e_pos - e_neg)/(e_pos + e_neg)
-
 def softmax(x, axis=-1):
     x_stable = x - x.max(axis=axis, keepdims=True)
     e = x_stable.exp()
     e_sum = e.sum(axis=axis, keepdims=True)
     return e / e_sum
+
+def cross_entropy(logits, targets, axis=-1):
+    probs = softmax(logits, axis=axis)
+    log_probs = probs.log()
+    loss_per_sample = -(targets * log_probs).sum(axis=axis)
+    batch_size = logits.data.shape[0]
+    return loss_per_sample.sum() / Tensor(batch_size, dtype=logits.dtype)
