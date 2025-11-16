222a9781581b499f0cb0b5cabbf6b1fddaab8932
VicBrienen <2x5chmddbj@privaterelay.appleid.com>
2025-11-16 13:34:05 +0100

    implement SGD optimizer

---
+++ microtorch/optim/SGD.py
@@ -0,0 +1,12 @@
+from optimizer import Optimizer
+
+class SGD(Optimizer):
+    def __init__(self, params, lr=1e-3):
+        super().__init__(params)
+        self.lr = lr
+
+    def step(self):
+        for p in self.params:
+            if p.grad is None:
+                continue
+            p.data -= self.lr * p.grad
--- microtorch/optim/optimizer.py
+++ microtorch/optim/optimizer.py
@@ -1,15 +1,7 @@
 class Optimizer:
-    def __init__(self, params, defaults):
-        self.param_groups = []
-        self.state = {}
-
-        for param in params:
-            self.param_groups.append({
-                'params': param,
-                **defaults
-            })
+    def __init__(self, params):
+        self.params = list(params)
 
     def zero_grad(self):
-        for group in self.param_groups:
-            for p in group['params']:
-                p.grad = None
+        for p in self.params:
+            p.grad = None
