cf72a5579e01581ca9385d0f8be1cb699bd2fdfa
VicBrienen <2x5chmddbj@privaterelay.appleid.com>
2025-12-09 19:55:53 +0100

    implement mathematically identical models using torch and microtorch library for later comparison

---
+++ torch_model.py
@@ -0,0 +1,23 @@
+import torch.nn as nn
+import torch.nn.functional as F
+
+class EfficientNet(nn.Module):
+    def __init__(self):
+        super().__init__()
+        # input size of CIFAR (Batch, 3, 32, 32)
+        self.conv1 = nn.Conv2d(3, 16, kernel_size=3, stride=2, padding=1) # (batch, 16, 16, 16)
+        self.conv2 = nn.Conv2d(16, 32, kernel_size=3, stride=2, padding=1) # (batch, 32, 8, 8)
+        self.conv3 = nn.Conv2d(32, 64, kernel_size=3, stride=2, padding=1) # (batch, 64, 4, 4)
+        self.linear1 = nn.Linear(1024, 128) # (batch, 128)
+        self.linear2 = nn.Linear(128, 10) # (batch, 10)
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        x = F.relu(self.conv3(x))
+
+        # flatten the feature map (batch, 64, 4, 4) -> (batch, 1024)
+        x = x.view(x.size(0), -1)
+
+        x = F.relu(self.linear1(x))
+        return self.linear2(x)
